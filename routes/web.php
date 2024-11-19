c4f3613c0f13e1ea7b0345e18cd855df992223ff
Sopi <sopiauranisa02@gmail.com>
2024-11-20 03:42:33 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KasirController;
+use App\Models\Customer;
 
 Route::get('/', function () {
     return view('welcome');    
@@ -9,4 +11,13 @@ Route::get('/', function () {
 Route::get('pesanan', function () {
     return view('pesanan');
 });
- 
+
+Route::get('/kasir/data-customer', function () {
+
+    // $customers = Customer::all();
+    // return view('kasir.data_customer', ['customers' => $customers]);
+    return view('kasir.data_customer');
+});
+
+Route::get('/kasir/data-customer', [KasirController::class, 'showCustomer'])->name('kasir.data_customer');
+
