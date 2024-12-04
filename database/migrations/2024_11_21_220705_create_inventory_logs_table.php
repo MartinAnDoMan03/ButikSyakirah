205ec2dfdf8123d14e543b7daffe92156dc9e263
unknown <martinadmanalu@gmail.com>
2024-12-04 08:52:42 +0700

    Deletion and foreign key fix

--- database/migrations/2024_11_21_220705_create_inventory_logs_table.php
+++ database/migrations/2024_11_21_220705_create_inventory_logs_table.php
@@ -17,14 +17,14 @@ return new class extends Migration
             $table->integer('transaction_id')->primary();
             $table->integer('stock_id');
             $table->string('reference_type');
-            $table->integer('reference_id');
+            $table->integer('order_reference_id')->nullable();
+            $table->integer('supplier_reference_id')->nullable();
             $table->string('transaction_type');
             $table->integer('quantity');
             $table->date('transaction_date');
             $table->foreign('stock_id')->references('stock_id')->on('stocks')->onDelete('restrict');
-            $table->foreign('reference_id')->references('order_id')->on('orders')->onDelete('restrict');
-            $table->foreign('reference_id')->references('supplier_id')->on('supplier')->onDelete('restrict');
-
+            $table->foreign('order_reference_id')->references('order_id')->on('orders')->onDelete('restrict');
+            $table->foreign('supplier_reference_id')->references('supplier_id')->on('suppliers')->onDelete('restrict');
         });
     }
 
