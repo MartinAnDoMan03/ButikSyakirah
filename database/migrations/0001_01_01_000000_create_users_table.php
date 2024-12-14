ebd667f7830f2faeeea257d84a9fddae9dd9934a
unknown <martinadmanalu@gmail.com>
2024-12-14 10:42:56 +0700

    user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('phone_number')->unique();
             $table->enum('role', ['penggunting', 'penjahit', 'pemayet'])->default('penggunting');
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('status', ['aktif,nonaktif']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
